6d92845bdca384b621185a96747599a80a017f0d
yung-khajiit <jeenriquez1219@gmail.com>
2024-02-19 08:31:14 -0600

    BaseTitle Working

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-  import NavMenu from '@/components/NavMenu.vue'
+
 </script>
 
 <template>
-  <NavMenu />
+ 
+
   <RouterView />
 </template>
---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex items-center justify-center gap-4">
+        <h1 class="text-6xl font-thin text-slate-700 uppercase">
+            <slot/>
+        </h1>
+
+    </div>
+
+
+</template>
